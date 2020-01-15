1c030f1e9580e63cc32918c6cf9e82418de87bc6
Jesse McKinney <jessman51386@gmail.com>
2020-01-15 17:43:10 -0500

    yay

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./NavBar.css";
 
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <Link to="/">
-          <b>Home</b>
-        </Link>
-        &nbsp;
-        <Link to="/contact-us">
-          <b>Contact Us</b>
-        </Link>
-      </nav>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link><Link to="/" className="home">Home</Link></Nav.Link>
+          <Nav.Link><Link to="/contact-us" className="contactUs">Contact Us</Link></Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
 
 export default NavBar;
+
+// <nav className="navbar">
+//         <Link to="/">
+//           <b>Home</b>
+//         </Link>
+//         &nbsp;
+//         <Link to="/contact-us">
+//           <b>Contact Us</b>
+//         </Link>
+//       </nav>
