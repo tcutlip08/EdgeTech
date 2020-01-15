da3264fd56408ea5878f28078a75c3b469d9c2d6
unknown <tcutlip08@gmail.com>
2020-01-15 17:21:12 -0500

    added blog page & route

---
+++ client/src/pages/Blog/Blog.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../../../components/NavBar/NavBar";
+import "./Blog.css";
+
+class JoinOurTeam extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <NavBar />
+        <div>Dis be Blog Page</div>
+      </div>
+    );
+  }
+}
+
+export default Blog;
