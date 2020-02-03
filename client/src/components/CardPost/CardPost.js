5812b3e26d581fecdb751299c20fd2314907e81e
unknown <tcutlip08@gmail.com>
2020-02-03 18:52:04 -0500

    creating cards for careers finding jobs/employees

---
+++ client/src/components/CardPost/CardPost.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./CardPost.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+const CardPost = props => {
+  return (
+    <Col className="cardPost" xs={3}>
+      {props.title}
+    </Col>
+  );
+};
+
+export default CardPost;
