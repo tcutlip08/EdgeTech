b21076da5945c08c6bae0e5c85ed02df9f8753b0
unknown <tcutlip08@gmail.com>
2020-02-03 03:26:32 -0500

    sneding data from form page to database

--- client/src/pages/Careers/FindingTalent/FindTalentPost/FindTalentPost.js
+++ client/src/pages/Careers/FindingTalent/FindTalentPost/FindTalentPost.js
@@ -5,20 +5,11 @@ import axios from "axios";
 
 class FindTalentPost extends Component {
   state = {
-    fname: "",
-    lname: "",
-    contactEmail: "",
-    contactPhone: "",
-    comment: "",
-    template_params: {
-      from_name: "",
-      from_message: "",
-      from_email: "",
-      from_phone: ""
-    },
-    service_id: "tcutlip08",
-    template_id: "template_8f4SePdi",
-    user_id: "user_yUEQjWRM02YZWiH5bCVvZ"
+    company: "",
+    jobTitle: "",
+    detail: "",
+    email: "",
+    phone: ""
   };
 
   componentDidMount() {}
@@ -28,10 +19,8 @@ class FindTalentPost extends Component {
       if (!this.state[property]) {
         return;
       }
-      console.log(`${property}: ${this.state[property]}`);
+      // console.log(`${property}: ${this.state[property]}`);
     }
-    // this.sendEmail();
-    this.clearData();
   }
 
   handleInputChange = event => {
@@ -43,13 +32,16 @@ class FindTalentPost extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    this.sendPost();
   };
 
-  sendEmail() {
+  sendPost() {
     axios
       .post("/api/LFEmp", this.state)
       .then(res => {
         console.log(res);
+        this.clearData();
       })
       .catch(err => {
         console.log(err);
@@ -70,75 +62,75 @@ class FindTalentPost extends Component {
               <div className="col-md-8 left-form">
                 <form method="post" id="contact" onSubmit={this.handleSubmit}>
                   <div className="form-group">
-                    <label className="sr-only" for="fname">
-                      First Name *
+                    <label className="sr-only" for="company">
+                      Company Name *
                     </label>
                     <input
                       className="required form-control"
-                      id="fname"
-                      name="fname"
-                      placeholder="First Name&nbsp;*"
-                      value={this.state.fname}
+                      id="company"
+                      name="company"
+                      placeholder="Company Name *"
+                      value={this.state.company}
                       onChange={this.handleInputChange}
                       type="text"
                       required
                     />
                   </div>
                   <div className="form-group">
-                    <label className="sr-only" for="lname">
-                      Last Name *
-                    </label>
-                    <input
-                      className="required form-control"
-                      id="lname"
-                      name="lname"
-                      placeholder="Last Name&nbsp;*"
-                      value={this.state.lname}
-                      onChange={this.handleInputChange}
-                      type="text"
-                      required
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label className="sr-only" for="contactEmail">
-                      Email *
-                    </label>
-                    <input
-                      className="required form-control h5-email"
-                      id="contactEmail"
-                      name="contactEmail"
-                      placeholder="Email&nbsp;*"
-                      value={this.state.contactEmail}
-                      onChange={this.handleInputChange}
-                      type="text"
-                      required
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label className="sr-only" for="contactPhone">
-                      Phone *
+                    <label className="sr-only" for="phone">
+                      Contact Number
                     </label>
                     <input
                       className="required form-control h5-phone"
-                      id="contactPhone"
-                      name="contactPhone"
-                      placeholder="Phone&nbsp;*"
-                      value={this.state.contactPhone}
+                      id="phone"
+                      name="phone"
+                      placeholder="Contact Number *"
+                      value={this.state.phone}
                       onChange={this.handleInputChange}
                       type="text"
                       required
                     />
                   </div>
                   <div className="form-group">
-                    <label className="sr-only" for="comment">
-                      Type your message here
+                    <label className="sr-only" for="email">
+                      Contact Email *
+                    </label>
+                    <input
+                      className="required form-control h5-email"
+                      id="email"
+                      name="email"
+                      placeholder="Contact Email *"
+                      value={this.state.email}
+                      onChange={this.handleInputChange}
+                      type="text"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="sr-only" for="jobTitle">
+                      Job Title *
+                    </label>
+                    <input
+                      className="required form-control"
+                      id="jobTitle"
+                      name="jobTitle"
+                      placeholder="Job Title *"
+                      value={this.state.jobTitle}
+                      onChange={this.handleInputChange}
+                      type="text"
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="sr-only" for="detail">
+                      Job Description
                     </label>
                     <textarea
                       className="required form-control"
-                      id="comment"
-                      name="comment"
-                      placeholder="Type your message here&nbsp;*"
-                      value={this.state.comment}
+                      id="detail"
+                      name="detail"
+                      placeholder="Type the Job Description here *"
+                      value={this.state.detail}
                       onChange={this.handleInputChange}
                       rows="6"
                       required
@@ -150,15 +142,15 @@ class FindTalentPost extends Component {
                 </form>
               </div>
               <div className="col-md-4 contact-info">
-                <div className="phone">
+                {/* <div className="phone">
                   <h2>Call</h2>
                   <a href="tel:1-800-867-5309">Call Now</a>
                 </div>
                 <div className="email">
                   <h2>Email</h2>
-                  {/* <a href="#">info@webcorpco.com</a> */}
+                  <a href="#">info@webcorpco.com</a>
                   <a href="mailto:info@webcorpco.com">info@webcorpco.com</a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
