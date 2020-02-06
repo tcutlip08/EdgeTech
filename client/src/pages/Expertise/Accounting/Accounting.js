a2afeb04b349978d81e435d27c15aafdef85fd72
Jesse McKinney <jessman51386@gmail.com>
2020-02-06 16:17:59 -0500

    yay accounting page/joinUs page/ landing page cool effects have been added

--- client/src/pages/Expertise/Accounting/Accounting.js
+++ client/src/pages/Expertise/Accounting/Accounting.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Zoom from "react-reveal/Zoom";
+import Flip from "react-reveal/Flip";
 import AccountingImage from "../../../assets/img/RandomImages/AI-accounting-finance-blog-min.jpg";
 // import "./Accounting.css";
 import "../Expertise.css";
@@ -42,7 +44,9 @@ class Accounting extends Component {
           <Container>
             <Row>
               <Col className="smallIcons">
-                <i className="far fa-money-bill-alt"></i>
+                <Flip delay={4000} duration={1000}>
+                  <i className="far fa-money-bill-alt"></i>
+                </Flip>
                 <Row className="headerSmallIcons">MoneyBillAlt</Row>
                 <Row>
                   <h4>
@@ -54,7 +58,9 @@ class Accounting extends Component {
                 </Row>
               </Col>
               <Col className="smallIcons">
-                <i className="fas fa-coins"></i>
+                <Flip delay={4000} duration={1000}>
+                  <i className="fas fa-coins"></i>
+                </Flip>
                 <Row className="headerSmallIcons">Coins</Row>
                 <Row>
                   <h4>
@@ -72,66 +78,76 @@ class Accounting extends Component {
         <Container id="HeaderWithBullets">
           <Row>
             <Col>
-              <h1 id="splitheader">Header</h1>
-              <p>
-                I'm Slim Shady, yes, I'm the real Shady. All you other Slim
-                Shadys are just imitating. So won't the real Slim Shady please
-                stand up. Please stand up, please stand up?. 'Cause I'm Slim
-                Shady, yes, I'm the real Shady. All you other Slim Shadys are
-                just imitating. So won't the real Slim Shady please stand up.
-                Please stand up, please stand up?When you walked through the
-                door. It was clear to me (Clear to me). You’re the one they
-                adore. Who they came to see (Who they came to see). You’re a
-                rock star (Baby). Everybody wants you (Everybody wants you).
-                Player, who can really blame you? (Who can really blame you?).
-                We’re the ones who made you.We're gonna rock this house until we
-                knock it down. So turn the volume loud. 'Cause it's mayhem 'til
-                the A.M.. So, baby, make just like K-Fed. And let yourself go,
-                let yourself go. Say "Fuck it!" before we kick the bucket.
-                Life's too short to not go for broke. So everybody, everybody,
-                go berserk, grab your vial, yeah.
-              </p>
+              <Zoom left cascade>
+                <h1 id="splitheader">Header</h1>
+                <p>
+                  I'm Slim Shady, yes, I'm the real Shady. All you other Slim
+                  Shadys are just imitating. So won't the real Slim Shady please
+                  stand up. Please stand up, please stand up?. 'Cause I'm Slim
+                  Shady, yes, I'm the real Shady. All you other Slim Shadys are
+                  just imitating. So won't the real Slim Shady please stand up.
+                  Please stand up, please stand up?When you walked through the
+                  door. It was clear to me (Clear to me). You’re the one they
+                  adore. Who they came to see (Who they came to see). You’re a
+                  rock star (Baby). Everybody wants you (Everybody wants you).
+                  Player, who can really blame you? (Who can really blame you?).
+                  We’re the ones who made you.We're gonna rock this house until
+                  we knock it down. So turn the volume loud. 'Cause it's mayhem
+                  'til the A.M.. So, baby, make just like K-Fed. And let
+                  yourself go, let yourself go. Say "Fuck it!" before we kick
+                  the bucket. Life's too short to not go for broke. So
+                  everybody, everybody, go berserk, grab your vial, yeah.
+                </p>
+              </Zoom>
             </Col>
 
             <Col>
               <Row>
                 <Col>
-                  <h1 id="splitheader">Header</h1>
-                  <ul>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                  </ul>
+                  <Zoom right cascade>
+                    <h1 id="splitheader">Header</h1>
+                    <ul>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                    </ul>
+                  </Zoom>
                 </Col>
                 <Col>
-                  <h1 id="splitheader">Header</h1>
-                  <ul>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                  </ul>
+                  <Zoom right cascade>
+                    <h1 id="splitheader">Header</h1>
+                    <ul>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                    </ul>
+                  </Zoom>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <h1 id="splitheader">Header</h1>
-                  <ul>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                  </ul>
+                  <Zoom right cascade>
+                    <h1 id="splitheader">Header</h1>
+                    <ul>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                    </ul>
+                  </Zoom>
                 </Col>
                 <Col>
-                  <h1 id="splitheader">Header</h1>
-                  <ul>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                    <li id="pointsOfInterest">something</li>
-                  </ul>
+                  <Zoom right cascade>
+                    <h1 id="splitheader">Header</h1>
+                    <ul>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                      <li id="pointsOfInterest">something</li>
+                    </ul>
+                  </Zoom>
                 </Col>
               </Row>
             </Col>
